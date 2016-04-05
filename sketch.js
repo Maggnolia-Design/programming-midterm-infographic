85a891fb5c153d8403b5ba862f7032502566ae6b
MistaFish31 <zhang.ma@husky.neu.edu>
2016-04-05 05:37:36 -0400

    spacing and max line

--- sketch.js
+++ sketch.js
@@ -78,37 +78,47 @@ function preload() {
 
 //Turning data into images
 function setup() {
-  createCanvas(2300,850)
+  createCanvas(2300,875)
   noStroke()
   background(28,28,28)
   
   //blue bars
-  var spacing=93
+  var spacing=103
   for (each in IQ_DATA) {
     var IQ_barLength=IQ_DATA[each]*1.5+60;
     spacing+=40
     
     fill('#66f')
-    rect(spacing, (height-100)-IQ_barLength, 15.2, IQ_barLength)
+    rect(spacing, (height-132)-IQ_barLength, 15.2, IQ_barLength)
   }
  
   //red bars 
-  var spacing=80
+  var spacing=90
   for (each in LIT_DATA) {
     var LIT_barLength=(LIT_DATA[each]*1.5+240);
     spacing+=40
     
     fill('#f66')
-    rect(spacing, (height-100)-LIT_barLength, 15.2, LIT_barLength)
+    rect(spacing, (height-132)-LIT_barLength, 15.2, LIT_barLength)
   }
 
   //labels
-  var spacing=80
+  var spacing=90
   for (var i=0; i<LIT_DATA.length; i++) {
     spacing+=40
     fill('#999')
     rect(spacing,753, 28, 28)
+
   }
+  
+  //LINES
+  fill('rgba(70%,70%,100%,0.5)')
+  rect(50,520,width,3)
+  text("107.0 IQ",50,515)
+  
+  fill('rgba(100%,70%,70%,0.5)')
+  rect(50,352,width,3)
+  text("100.0 Literacy",50,347)
   
 
   //TITLE
@@ -116,35 +126,35 @@ function setup() {
   textSize(55)
   fill('#999')
   textFont(fontHeader)
-  text("DISPARATE REPRESENTATION",100,85,600,200)
+  text("DISPARATE REPRESENTATION",110,85,600,200)
   
   //LABELS
   fill(28,28,28)
   textSize(17)
-  text("BE / FR / BG / DK / BB / HR / ET /\tDE / JP / HU /", 125,753,1000,200)
-  text("HK / BR \tGN \t FI /\tFJ / GR / NL \tNG /CO",522,753,1000,200)
-  text("/ NZ / NP / EG / GT\tCN",867,753,1000,200)
-  text("/ GQ / IQ / CA /\tIR /\tIT /\tEC/ CD / CZ / AR \tAU /\tIL /\tIN /\tLB /\tIE /\tID / CU /GH",1067,753,1000,200)
-  text("/ MA\t KE / JM /MN \tMH/ KR / MY \tMX / AT",1750,753,1000,200)
+  text("BE / FR / BG / DK / BB / HR / ET /\tDE / JP / HU /", 135,753,1000,200)
+  text("HK / BR \tGN \t FI /\tFJ / GR / NL \tNG /CO",532,753,1000,200)
+  text("/ NZ / NP / EG / GT\tCN",877,753,1000,200)
+  text("/ GQ / IQ / CA /\tIR /\tIT /\tEC/ CD / CZ / AR \tAU /\tIL /\tIN /\tLB /\tIE /\tID / CU /GH",1077,753,1000,200)
+  text("/ MA\t KE / JM /MN \tMH/ KR / MY \tMX / AT",1760,753,1000,200)
   
   //SUBTITLE
   fill('#444')
   textSize(100)
-  text("[                ]",680,60,800,200)
+  text("[                ]",690,60,800,200)
 
   fill('#777')
   textSize(26)
   textFont(fontBody)
-  text("Do countries with low literacy rates also show disproportionately low IQ averages?",740,100,600,200)
+  text("Do countries with low literacy rates also show disproportionately low IQ averages?",750,100,600,200)
   
   //KEY
   textSize(17)
   fill('#66f')
-  text("IQ Level",760,250,500,200)
-  rect(740,260,10,8)
+  text("IQ Level",770,250,500,200)
+  rect(750,260,10,8)
   fill('#f66')
-  text("Literacy Rate",760,225,500,200)
-  rect(740,235,10,8)
+  text("Literacy Rate",770,225,500,200)
+  rect(750,235,10,8)
 
   
 //GRAPH
