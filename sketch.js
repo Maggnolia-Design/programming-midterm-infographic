1d862ed67cabb12ea75cfa9995643a74eec2463c
Maggie Zhang <maggie.zhang.us@gmail.com>
2023-03-22 19:21:28 -0400

    fix fontHeader and labels
    Replaced Quicksand with PT Serif for header typography. Adjusted labels. Temporary solution.

--- sketch.js
+++ sketch.js
@@ -1,21 +1,281 @@
 //  PIE_DATA = {"data": [["Other", 0.0]], "title": "Education > Average IQ, 2002"};
-  IQ_DATA_OG = {"data": {"BE": 100.0, "FR": 98.0, "BG": 93.0, "DK": 98.0, "BB": 78.0, "HR": 90.0, "ET": 63.0, "DE": 102.0, "JP": 105.0, "HU": 99.0, "HK": 107.0, "BR": 87.0, "GN": 66.0, "FI": 97.0, "FJ": 84.0, "GR": 93.0, "NL": 102.0, "NG": 67.0, "NZ": 100.0, "NP": 78.0, "EG": 83.0, "GT": 79.0, "CO": 89.0, "CN": 100.0, "GQ": 59.0, "IQ": 87.0, "CA": 97.0, "IR": 84.0, "IT": 102.0, "EC": 80.0, "CD": 65.0, "CZ": 97.0, "AR": 96.0, "AU": 98.0, "IL": 94.0, "IN": 81.0, "LB": 86.0, "IE": 93.0, "ID": 89.0, "CU": 85.0, "GH": 71.0, "MA": 85.0, "KE": 72.0, "JM": 72.0, "MN": 98.0, "MH": 84.0, "KR": 106.0, "MY": 92.0, "MX": 87.0, "AT": 102.0}};
-  STAT_DIGITS = 2;
-  STAT_PREFIX = '';
-  STAT_POSTFIX = '';
+IQ_DATA_OG = {
+  data: {
+    BE: 100.0,
+    FR: 98.0,
+    BG: 93.0,
+    DK: 98.0,
+    BB: 78.0,
+    HR: 90.0,
+    ET: 63.0,
+    DE: 102.0,
+    JP: 105.0,
+    HU: 99.0,
+    HK: 107.0,
+    BR: 87.0,
+    GN: 66.0,
+    FI: 97.0,
+    FJ: 84.0,
+    GR: 93.0,
+    NL: 102.0,
+    NG: 67.0,
+    NZ: 100.0,
+    NP: 78.0,
+    EG: 83.0,
+    GT: 79.0,
+    CO: 89.0,
+    CN: 100.0,
+    GQ: 59.0,
+    IQ: 87.0,
+    CA: 97.0,
+    IR: 84.0,
+    IT: 102.0,
+    EC: 80.0,
+    CD: 65.0,
+    CZ: 97.0,
+    AR: 96.0,
+    AU: 98.0,
+    IL: 94.0,
+    IN: 81.0,
+    LB: 86.0,
+    IE: 93.0,
+    ID: 89.0,
+    CU: 85.0,
+    GH: 71.0,
+    MA: 85.0,
+    KE: 72.0,
+    JM: 72.0,
+    MN: 98.0,
+    MH: 84.0,
+    KR: 106.0,
+    MY: 92.0,
+    MX: 87.0,
+    AT: 102.0,
+  },
+};
+STAT_DIGITS = 2;
+STAT_PREFIX = "";
+STAT_POSTFIX = "";
 
-  
 //  PIE_DATA = {"data": [["Other", 0.0]], "title": "Education > Literacy > Total population, 2011"};
-  LIT_DATA_OG = {"data": {"BD": 43.1, "BE": 99.0, "BF": 21.8, "BG": 98.6, "BA": 96.7, "BB": 99.7, "WF": 50.0, "BM": 98.0, "BN": 91.8, "BO": 87.2, "BH": 89.1, "BI": 51.6, "BJ": 34.7, "BT": 47.0, "JM": 87.9, "JO": 89.9, "WS": 99.7, "BR": 88.6, "BS": 95.6, "BY": 99.6, "BZ": 94.1, "LV": 99.8, "RW": 70.4, "TL": 58.6, "TM": 98.8, "TJ": 99.4, "RO": 98.4, "GW": 42.4, "GU": 99.0, "GT": 70.6, "GR": 97.5, "GQ": 85.7, "JP": 99.0, "GY": 98.8, "GE": 100.0, "GD": 96.0, "GB": 99.0, "GA": 63.2, "GN": 29.5, "GM": 40.1, "GL": 100.0, "GH": 74.8, "OM": 81.4, "BW": 81.2, "HR": 98.5, "HT": 52.9, "SV": 81.1, "HK": 94.0, "HN": 76.2, "AD": 100.0, "PR": 94.1, "PW": 92.0, "PT": 93.3, "PY": 94.0, "AI": 95.0, "PA": 92.6, "PF": 98.0, "PG": 66.0, "PE": 92.9, "PK": 49.9, "PH": 95.9, "PL": 99.8, "PM": 99.0, "ZM": 80.6, "RU": 99.6, "EE": 99.8, "EG": 71.4, "ZA": 86.4, "EC": 92.5, "AL": 86.5, "AO": 67.4, "ET": 42.7, "SO": 37.8, "ZW": 90.7, "KY": 98.0, "ES": 97.9, "ER": 58.6, "MD": 99.1, "MG": 68.9, "UY": 98.0, "MC": 99.0, "UZ": 99.3, "MM": 89.9, "ML": 46.4, "MN": 99.1, "MH": 93.7, "US": 99.0, "MU": 85.6, "MT": 92.8, "MW": 62.7, "MV": 93.8, "MQ": 97.7, "MP": 97.0, "MS": 97.0, "MR": 41.7, "UG": 69.9, "MY": 88.9, "MX": 86.1, "IL": 97.1, "FR": 99.0, "MA": 52.3, "FI": 100.0, "FJ": 93.7, "FM": 89.0, "NI": 67.5, "NL": 99.0, "NO": 100.0, "NA": 84.0, "VU": 74.0, "NC": 96.2, "NE": 28.7, "NG": 68.0, "NZ": 99.0, "NP": 45.2, "NU": 95.0, "CK": 95.0, "CI": 50.9, "CH": 99.0, "CO": 90.4, "CN": 91.6, "CM": 79.0, "CL": 96.2, "CA": 99.0, "CF": 51.0, "CD": 65.5, "CZ": 99.0, "CY": 97.6, "CR": 96.0, "HU": 99.4, "CV": 76.6, "CU": 97.0, "SZ": 81.6, "SY": 79.6, "KG": 98.7, "KE": 85.1, "SR": 89.6, "KH": 73.6, "KN": 97.8, "KM": 56.5, "ST": 84.9, "SK": 99.6, "KR": 98.1, "SI": 99.7, "KP": 99.0, "KW": 93.3, "SN": 40.2, "SM": 96.0, "SL": 35.1, "SC": 91.9, "KZ": 99.5, "SA": 78.8, "SG": 93.2, "SE": 99.0, "SD": 61.1, "DO": 84.7, "DM": 94.0, "DJ": 67.9, "DK": 99.0, "DE": 99.0, "YE": 50.2, "DZ": 70.0, "LB": 87.4, "LC": 90.1, "LA": 73.0, "TW": 96.1, "TT": 98.6, "TR": 87.4, "LK": 92.3, "LI": 100.0, "TN": 74.3, "TO": 98.9, "LT": 99.6, "LU": 100.0, "LR": 57.5, "LS": 84.8, "TH": 96.0, "TG": 60.9, "TD": 47.5, "TC": 98.0, "LY": 82.6, "VA": 100.0, "VC": 96.0, "AE": 77.9, "VE": 93.4, "AG": 85.8, "AF": 28.1, "IQ": 40.4, "IS": 99.0, "IR": 79.4, "AM": 98.6, "IT": 98.6, "VN": 94.0, "AS": 97.0, "AR": 97.1, "AU": 99.0, "AT": 98.0, "AW": 97.3, "IN": 59.5, "TZ": 78.2, "AZ": 98.8, "IE": 99.0, "ID": 90.4, "UA": 99.7, "QA": 89.0, "MZ": 47.8}};
-  STAT_DIGITS = 2;
-  STAT_PREFIX = '';
-  STAT_POSTFIX = '%';
+LIT_DATA_OG = {
+  data: {
+    BD: 43.1,
+    BE: 99.0,
+    BF: 21.8,
+    BG: 98.6,
+    BA: 96.7,
+    BB: 99.7,
+    WF: 50.0,
+    BM: 98.0,
+    BN: 91.8,
+    BO: 87.2,
+    BH: 89.1,
+    BI: 51.6,
+    BJ: 34.7,
+    BT: 47.0,
+    JM: 87.9,
+    JO: 89.9,
+    WS: 99.7,
+    BR: 88.6,
+    BS: 95.6,
+    BY: 99.6,
+    BZ: 94.1,
+    LV: 99.8,
+    RW: 70.4,
+    TL: 58.6,
+    TM: 98.8,
+    TJ: 99.4,
+    RO: 98.4,
+    GW: 42.4,
+    GU: 99.0,
+    GT: 70.6,
+    GR: 97.5,
+    GQ: 85.7,
+    JP: 99.0,
+    GY: 98.8,
+    GE: 100.0,
+    GD: 96.0,
+    GB: 99.0,
+    GA: 63.2,
+    GN: 29.5,
+    GM: 40.1,
+    GL: 100.0,
+    GH: 74.8,
+    OM: 81.4,
+    BW: 81.2,
+    HR: 98.5,
+    HT: 52.9,
+    SV: 81.1,
+    HK: 94.0,
+    HN: 76.2,
+    AD: 100.0,
+    PR: 94.1,
+    PW: 92.0,
+    PT: 93.3,
+    PY: 94.0,
+    AI: 95.0,
+    PA: 92.6,
+    PF: 98.0,
+    PG: 66.0,
+    PE: 92.9,
+    PK: 49.9,
+    PH: 95.9,
+    PL: 99.8,
+    PM: 99.0,
+    ZM: 80.6,
+    RU: 99.6,
+    EE: 99.8,
+    EG: 71.4,
+    ZA: 86.4,
+    EC: 92.5,
+    AL: 86.5,
+    AO: 67.4,
+    ET: 42.7,
+    SO: 37.8,
+    ZW: 90.7,
+    KY: 98.0,
+    ES: 97.9,
+    ER: 58.6,
+    MD: 99.1,
+    MG: 68.9,
+    UY: 98.0,
+    MC: 99.0,
+    UZ: 99.3,
+    MM: 89.9,
+    ML: 46.4,
+    MN: 99.1,
+    MH: 93.7,
+    US: 99.0,
+    MU: 85.6,
+    MT: 92.8,
+    MW: 62.7,
+    MV: 93.8,
+    MQ: 97.7,
+    MP: 97.0,
+    MS: 97.0,
+    MR: 41.7,
+    UG: 69.9,
+    MY: 88.9,
+    MX: 86.1,
+    IL: 97.1,
+    FR: 99.0,
+    MA: 52.3,
+    FI: 100.0,
+    FJ: 93.7,
+    FM: 89.0,
+    NI: 67.5,
+    NL: 99.0,
+    NO: 100.0,
+    NA: 84.0,
+    VU: 74.0,
+    NC: 96.2,
+    NE: 28.7,
+    NG: 68.0,
+    NZ: 99.0,
+    NP: 45.2,
+    NU: 95.0,
+    CK: 95.0,
+    CI: 50.9,
+    CH: 99.0,
+    CO: 90.4,
+    CN: 91.6,
+    CM: 79.0,
+    CL: 96.2,
+    CA: 99.0,
+    CF: 51.0,
+    CD: 65.5,
+    CZ: 99.0,
+    CY: 97.6,
+    CR: 96.0,
+    HU: 99.4,
+    CV: 76.6,
+    CU: 97.0,
+    SZ: 81.6,
+    SY: 79.6,
+    KG: 98.7,
+    KE: 85.1,
+    SR: 89.6,
+    KH: 73.6,
+    KN: 97.8,
+    KM: 56.5,
+    ST: 84.9,
+    SK: 99.6,
+    KR: 98.1,
+    SI: 99.7,
+    KP: 99.0,
+    KW: 93.3,
+    SN: 40.2,
+    SM: 96.0,
+    SL: 35.1,
+    SC: 91.9,
+    KZ: 99.5,
+    SA: 78.8,
+    SG: 93.2,
+    SE: 99.0,
+    SD: 61.1,
+    DO: 84.7,
+    DM: 94.0,
+    DJ: 67.9,
+    DK: 99.0,
+    DE: 99.0,
+    YE: 50.2,
+    DZ: 70.0,
+    LB: 87.4,
+    LC: 90.1,
+    LA: 73.0,
+    TW: 96.1,
+    TT: 98.6,
+    TR: 87.4,
+    LK: 92.3,
+    LI: 100.0,
+    TN: 74.3,
+    TO: 98.9,
+    LT: 99.6,
+    LU: 100.0,
+    LR: 57.5,
+    LS: 84.8,
+    TH: 96.0,
+    TG: 60.9,
+    TD: 47.5,
+    TC: 98.0,
+    LY: 82.6,
+    VA: 100.0,
+    VC: 96.0,
+    AE: 77.9,
+    VE: 93.4,
+    AG: 85.8,
+    AF: 28.1,
+    IQ: 40.4,
+    IS: 99.0,
+    IR: 79.4,
+    AM: 98.6,
+    IT: 98.6,
+    VN: 94.0,
+    AS: 97.0,
+    AR: 97.1,
+    AU: 99.0,
+    AT: 98.0,
+    AW: 97.3,
+    IN: 59.5,
+    TZ: 78.2,
+    AZ: 98.8,
+    IE: 99.0,
+    ID: 90.4,
+    UA: 99.7,
+    QA: 89.0,
+    MZ: 47.8,
+  },
+};
+STAT_DIGITS = 2;
+STAT_PREFIX = "";
+STAT_POSTFIX = "%";
 
-  IQ_DATA = [] 
-  LIT_DATA =[]
-  COUNTRIES = []
+IQ_DATA = [];
+LIT_DATA = [];
+COUNTRIES = [];
 
-  COUNTRY_CODES=[
+COUNTRY_CODES = [
   "Belgium",
   "France",
   "Bulgaria",
@@ -65,9 +325,8 @@
   "South Korea",
   "Malaysia",
   "Mexico",
-  "Austria"
-]
-  
+  "Austria",
+];
 
 //A function that sorts literacy rates from low to high
 
@@ -82,7 +341,7 @@
 //         temp = entry1
 //         entry1 = entry2
 //         entry2 = temp
-        
+
 //         temp = LIT_DATA_OG.data[entry1]
 //         LIT_DATA_OG.data[entry1] = LIT_DATA_OG.data[entry2]
 //         LIT_DATA_OG.data[entry2] = temp
@@ -94,147 +353,171 @@
 // sort(LIT_DATA_OG.data)
 //console.log(LIT_DATA_OG.data)
 
-
 //A function that looks for pairs of strings...
 //then puts the corresponding data into a 3 arrays...
 //one for IQ values, one for literacy calues, one for country names
 
 var hit = false;
-var compare
+var compare;
 
-compare=function(){
-  for (entry1 in IQ_DATA_OG.data){
-    for(entry2 in LIT_DATA_OG.data){
-      if(entry1==entry2){
-        IQ_DATA.push(IQ_DATA_OG.data[entry1])
-        LIT_DATA.push(LIT_DATA_OG.data[entry2])
-        COUNTRIES.push(entry1)
-        break
+compare = function () {
+  for (entry1 in IQ_DATA_OG.data) {
+    for (entry2 in LIT_DATA_OG.data) {
+      if (entry1 == entry2) {
+        IQ_DATA.push(IQ_DATA_OG.data[entry1]);
+        LIT_DATA.push(LIT_DATA_OG.data[entry2]);
+        COUNTRIES.push(entry1);
+        break;
       }
     }
   }
-}
+};
 
-compare()
+compare();
 //console.log(IQ_DATA)
 // console.log(LIT_DATA)
 // console.log(COUNTRIES)
 
 //Loading typefaces
 
-var fontHeader, fontBody
+let fontHeader;
+// var fontBody;
 
 function preload() {
-   fontHeader = loadFont("fonts/Quicksand-Regular.ttf");
-   fontBody = loadFont("fonts/EBGaramond-Regular.ttf");
+  fontHeader = "PT Serif";
+  // fontBody = loadFont("fonts/EBGaramond-Regular.ttf");
 }
-
 
 //Turning data into images
 function setup() {
-  createCanvas(2300,875)
-  noStroke()
-  background(28,28,28)
-  
+  createCanvas(2300, 875);
+  noStroke();
+  background(28, 28, 28);
+
   //blue bars
-  var spacing=103
+  var spacing = 103;
   for (each in IQ_DATA) {
-    var IQ_barLength=IQ_DATA[each]*1.5+60;
-    spacing+=40
-    
-    fill('#66f')
-    rect(spacing, (height-132)-IQ_barLength, 15.2, IQ_barLength)
-  }
- 
-  //red bars 
-  var spacing=90
-  for (each in LIT_DATA) {
-    var LIT_barLength=(LIT_DATA[each]*1.5+240);
-    spacing+=40
-    
-    fill('#f66')
-    rect(spacing, (height-132)-LIT_barLength, 15.2, LIT_barLength)
+    var IQ_barLength = IQ_DATA[each] * 1.5 + 60;
+    spacing += 40;
+
+    fill("#66f");
+    rect(spacing, height - 132 - IQ_barLength, 15.2, IQ_barLength);
   }
 
-  
+  //red bars
+  var spacing = 90;
+  for (each in LIT_DATA) {
+    var LIT_barLength = LIT_DATA[each] * 1.5 + 240;
+    spacing += 40;
+
+    fill("#f66");
+    rect(spacing, height - 132 - LIT_barLength, 15.2, LIT_barLength);
+  }
+
   //LINES
-  fill('rgba(70%,70%,100%,0.5)')
-  rect(50,520,width,3)
-  text("107.0 IQ",50,515)
-  
-  fill('rgba(100%,70%,70%,0.5)')
-  rect(50,352,width,3)
-  text("100.0 Literacy",50,347)
-  
+  fill("rgba(70%,70%,100%,0.5)");
+  rect(50, 520, width, 3);
+  text("107.0 IQ", 50, 515);
+
+  fill("rgba(100%,70%,70%,0.5)");
+  rect(50, 352, width, 3);
+  text("100.0 Literacy", 50, 347);
 
   //TITLE
-  noStroke()
-  textSize(55)
-  fill('#999')
-  textFont(fontHeader)
-  text("DISPARATE REPRESENTATION",110,85,600,200)
+  noStroke();
+  textSize(55);
+  fill("#999");
+  textFont(fontHeader);
+  text("CHALLENGING PREJUDICE", 110, 85, 600, 200);
 
-  
   //SUBTITLE
-  fill('#444')
-  textSize(100)
-  text("[                ]",690,60,800,200)
+  fill("#444");
+  textSize(100);
+  text("[                ]", 690, 60, 800, 200);
 
-  fill('#777')
-  textSize(26)
-  textFont(fontBody)
-  text("Do countries with low literacy rates also show disproportionately low IQ averages?",750,100,600,200)
-  
+  fill("#777");
+  textSize(26);
+  // textFont(fontBody);
+  text(
+    "Is there a coorelation between literacy rates and IQ?",
+    740,
+    87,
+    400,
+    200
+  );
+
   //KEY
-  textSize(17)
-  fill('#66f')
-  text("IQ Level",770,250,500,200)
-  rect(750,260,10,8)
-  fill('#f66')
-  text("Literacy Rate",770,225,500,200)
-  rect(750,235,10,8)
+  textSize(17);
+  fill("#66f");
+  text("IQ Level", 770, 257, 500, 200);
+  rect(750, 260, 10, 8);
+  fill("#f66");
+  text("Literacy Rate", 770, 232, 500, 200);
+  rect(750, 235, 10, 8);
 
-  
-//GRAPH
-//  noFill()
-//  stroke('#555')
-//  rect(70,250,950,400)
+  //GRAPH
+  //  noFill()
+  //  stroke('#555')
+  //  rect(70,250,950,400)
 
   //labels
-  var spacing=90
-  for (var i=0; i<LIT_DATA.length; i++) {
-    spacing+=40
-    fill('#999')
-    rect(spacing,753, 28, 28)
+  var spacing = 90;
+  for (var i = 0; i < LIT_DATA.length; i++) {
+    spacing += 40;
+    fill("#999");
+    rect(spacing, 753, 28, 28);
   }
 
   //LABELS
-  fill(28,28,28)
-  textFont(fontHeader)
-  textSize(17)
-  text("BE / FR / BG / DK / BB / HR / ET /\tDE / JP / HU /", 135,753,1000,200)
-  text("HK / BR \tGN \t FI /\tFJ / GR / NL \tNG /CO",532,753,1000,200)
-  text("/ NZ / NP / EG / GT\tCN",877,753,1000,200)
-  text("/ GQ / IQ / CA /\tIR /\tIT /\tEC/ CD / CZ / AR \tAU /\tIL /\tIN /\tLB /\tIE /\tID / CU /GH",1077,753,1000,200)
-  text("/ MA\t KE / JM /MN \tMH/ KR / MY \tMX / AT",1760,753,1000,200)
-
+  fill(28, 28, 28);
+  textFont(fontHeader);
+  textSize(17.5);
+  text(
+    "BE /  FR /  BG /  DK /  BB / HR /  ET /  DE /  JP  / HU /",
+    133,
+    760,
+    1000,
+    200
+  );
+  text(
+    "HK / BR / GN  /  FI /   FJ  / GR /  NL / NG  / CO",
+    535,
+    760,
+    1000,
+    200
+  );
+  text("NZ / NP  / EG /  GT / CN", 893, 760, 1000, 200);
+  text(
+    "GQ  / IQ /  CA  /  IR  /  IT /  EC /  CD    CZ    AR / AU  /  IL  / IN  /\tLB /   IE /   ID /  CU / GH",
+    1090,
+    760,
+    1000,
+    200
+  );
+  text("/ MA    KE  / JM / MN   MH   KR    MY / MX / AT", 1760, 760, 1000, 200);
 }
 
 function draw() {
-  var spacing=90
-//collision with labels
-  setup()
-  for (var i=0; i<LIT_DATA.length; i++) {
-    spacing+=40
-    hit=collidePointRect(mouseX,mouseY,spacing,753, 28, 28)
-    if (hit){
-      fill('#aaa')
-      text(COUNTRY_CODES[i] + ": " + " Literacy rate of " + LIT_DATA[i] + "; IQ level of " + IQ_DATA[i],spacing,810)
+  var spacing = 90;
+  //collision with labels
+  setup();
+  for (var i = 0; i < LIT_DATA.length; i++) {
+    spacing += 40;
+    hit = collidePointRect(mouseX, mouseY, spacing, 753, 28, 28);
+    if (hit) {
+      fill("#aaa");
+      text(
+        COUNTRY_CODES[i] +
+          ": " +
+          " Literacy rate of " +
+          LIT_DATA[i] +
+          "; IQ level of " +
+          IQ_DATA[i],
+        spacing,
+        810
+      );
     }
   }
 }
 
-console.log(COUNTRIES)
-  
-
-
+console.log(COUNTRIES);
